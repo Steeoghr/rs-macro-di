ae812ed512d41b06d39f7ef528e6e3069514beea
steo <stefano.gherardi.fl@gmail.com>
2024-06-14 10:02:02 +0200

    init webhost

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,8 @@
 
 pub mod provider;
 pub mod macros;
+pub mod host;
+pub mod routes;
 
 #[cfg(test)]
 mod tests;
---
+++ src/routes.rs
@@ -0,0 +1,77 @@
+
+
+
+
+
+
+extern crate proc_macro;
+
+use proc_macro::TokenStream;
+use quote::quote;
+use syn::{parse_macro_input, AttributeArgs, FnArg, ItemFn, Lit, Meta, MetaNameValue, NestedMeta, PatType};
+
+#[proc_macro_attribute]
+pub fn get(args: TokenStream, input: TokenStream) -> TokenStream {
+    generate_route("GET", args, input, false)
+}
+
+#[proc_macro_attribute]
+pub fn post(args: TokenStream, input: TokenStream) -> TokenStream {
+    generate_route("POST", args, input, true)
+}
+
+fn generate_route(method: &str, args: TokenStream, input: TokenStream, has_body: bool) -> TokenStream {
+    let args = parse_macro_input!(args as AttributeArgs);
+    let input = parse_macro_input!(input as ItemFn);
+    let path = match args.get(0) {
+        Some(NestedMeta::Lit(Lit::Str(lit))) => lit.value(),
+        _ => panic!("Expected path as first argument"),
+    };
+    let params = extract_params_from_path(&path);
+
+    let name = &input.sig.ident;
+    let vis = &input.vis;
+    let block = &input.block;
+
+    // Estrai i parametri dal metodo
+    let method_params: Vec<_> = input.sig.inputs.iter().map(|arg| {
+        if let FnArg::Typed(PatType { pat, ty, .. }) = arg {
+            let pat_str = quote!(#pat).to_string();
+            let ty_str = quote!(#ty).to_string();
+            (pat_str, ty_str)
+        } else {
+            panic!("Expected typed arguments");
+        }
+    }).collect();
+
+    let expanded = quote! {
+        #vis fn #name(params: &[(&str, &str)], body: Option<String>) -> String {
+            #block
+        }
+
+        // Store the route information
+        inventory::submit! {
+            crate::Route {
+                method: #method.to_string(),
+                path: format!("{}{}", <Self as Controller>::base_path(), #path),
+                handler: #name,
+                params: vec![#(#params.to_string()),*],
+                has_body: #has_body,
+            }
+        }
+    };
+
+    expanded.into()
+}
+
+fn extract_params_from_path(path: &str) -> Vec<String> {
+    path.split('/')
+        .filter_map(|segment| {
+            if segment.starts_with('{') && segment.ends_with('}') {
+                Some(segment.trim_matches(&['{', '}'][..]).to_string())
+            } else {
+                None
+            }
+        })
+        .collect()
+}
